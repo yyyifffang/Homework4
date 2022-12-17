117ffb803a60fb029721b1affc8da0378f76b85e
yyyifffang <Fragrant115@gmail.com>
2022-12-17 18:51:10 +0800

    first commit

---
+++ chapter12/app/src/main/java/com/example/chapter12kt/SecActivity.kt
@@ -0,0 +1,11 @@
+package com.example.chapter12kt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sec)
+    }
+}
